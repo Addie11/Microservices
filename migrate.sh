6ac6aba39578e21a9e8cb5ec5234096207db8a49
addie11 <adityarawat118@gmail.com>
2023-05-17 10:49:02 +0200

    Added Docker Compose File

--- migrate.sh
+++ migrate.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL:-"adityarawat118@gmail.com"}
 cd /app/
-/opt/venv/bin/python manape.py migrate --noinput
-/opt/venv/bin/python manape.py createsuperuser --email $SUPERUSER_EMAIL --noinput || true
+echo "Applying Migrations"
+/opt/venv/bin/python manage.py migrate --noinput
+echo "Applying Migrations 2"
+/opt/venv/bin/python manage.py createsuperuser --email $SUPERUSER_EMAIL --noinput || true
 
