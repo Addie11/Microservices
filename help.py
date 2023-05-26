504c1bc494b0d3aa82c5e9c11a17536e64683960
addie11 <adityarawat118@gmail.com>
2023-05-26 15:23:31 +0200

    Modified Message

--- help.py
+++ help.py
@@ -1,3 +1,15 @@
 import pathlib
+import secrets
+import string
 
 print(pathlib.Path(__file__).parent)
+
+print(string.ascii_letters)
+print(string.digits)
+print(string.punctuation)
+password_generator = string.ascii_letters + string.digits + string.punctuation
+print(secrets.choice(string.ascii_letters))
+print("".join(secrets.choice(password_generator) for _ in range(20)))
+
+if secrets.compare_digest("abc", "abc"):
+    print("same")
