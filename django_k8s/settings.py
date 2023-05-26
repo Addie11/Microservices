6c6ccdca80933c8cc46e0cb59457237feabae989
addie11 <adityarawat118@gmail.com>
2023-05-26 16:36:35 +0200

    Modified Message

--- django_k8s/settings.py
+++ django_k8s/settings.py
@@ -91,6 +91,7 @@ DB_IS_AVAIL = all([
     DB_HOST,
     DB_PORT
 ])
+DB_IGNORE_SSL=os.environ.get("DB_IGNORE_SSL") == "true"
 
 if DB_IS_AVAIL:
     DATABASES = {
@@ -103,6 +104,11 @@ if DB_IS_AVAIL:
             "PORT": DB_PORT,
         }
     }
+
+    if not DB_IGNORE_SSL:
+        DATABASES["default"]["OPTIONS"] = {
+            "sslmode": "require"
+        }
 
 print(DATABASES)
 # Password validation
